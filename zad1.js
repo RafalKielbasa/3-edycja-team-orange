32268664c76dbc500acad29b28840893a9728fe1
Poul <paweljarujaruszewski@gmail.com>
2022-12-17 20:37:02 +0100

    first try

--- zad1.js
+++ zad1.js
@@ -1,1 +1,47 @@
-console.log("test");
+const people = [
+  {
+    firstName: "Bartolomeo",
+    lastName: "Lozano",
+  },
+  {
+    firstName: "Mateo",
+    lastName: "Loza",
+  },
+];
+
+function createNickName(firstName, lastName) {
+  const threeCharsOfName = firstName.slice(0, 3).split("").reverse().join("");
+  const threeCharsOfLastName = lastName.slice(-3).split("").reverse().join("");
+
+  const nickName = [threeCharsOfName, threeCharsOfLastName].join("");
+  return nickName.charAt(0).toUpperCase() + nickName.slice(1).toLowerCase();
+}
+
+// const createNickName = people.map((person) => {
+//   const threeCharsOfName = person.firstName
+//     .slice(0, 3)
+//     .split("")
+//     .reverse()
+//     .join("");
+
+//   const threeCharsOfLastName = person.lastName
+//     .slice(-3)
+//     .split("")
+//     .reverse()
+//     .join("");
+
+//   const nickName = [threeCharsOfName, threeCharsOfLastName].join("");
+
+//   return nickName.charAt(0).toUpperCase() + nickName.slice(1).toLowerCase();
+// });
+
+const peopleWithUsurname = people.map((person) => {
+  return {
+    fistName: person.firstName,
+    lastName: person.lastName,
+    nickname: createNickName(person.firstName, person.lastName),
+  };
+});
+
+console.log(peopleWithUsurname);
+// console.log(peopleMapped)
