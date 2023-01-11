66535b2fc5c7cd0f23ce098e1cfac54808767265
PiotrKurzy <pkurzydym@gmail.com>
2023-01-11 17:30:20 +0100

    homework

---
+++ PD-PiotrKurzydym/script.js
@@ -0,0 +1,11 @@
+fetch({
+  people: `https://swapi.dev/api/people/`,
+  planets: `https://swapi.dev/api/planets/`,
+  films: `https://swapi.dev/api/films/`,
+  species: `https://swapi.dev/api/species/`,
+  vehicles: `https://swapi.dev/api/vehicles/`,
+  starships: `https://swapi.dev/api/starships/`,
+});
+
+//   .then((response) => response.json())
+// .then((data) => console.log(data));
