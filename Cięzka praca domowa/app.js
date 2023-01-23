26a472730070985c3c76df0bace456d1a2a36639
Poul <paweljarujaruszewski@gmail.com>
2023-01-23 22:03:22 +0100

    better

--- Cięzka praca domowa/app.js
+++ Cięzka praca domowa/app.js
@@ -3,17 +3,21 @@ const container = document.getElementById("header");
 
 const data = fetch(api)
   .then((response) => response.json())
-  .then((data) => {
-    Object.entries(data).forEach((item) => {
-      const button = document.createElement("button");
-      button.addEventListener("click", async () => {
-        const response = await fetch(item[1]);
-        const data = await response.json();
-        console.clear();
-        console.log(data);
-      });
-
-      container.appendChild(button);
-      button.innerText = item[0].toUpperCase();
-    });
+  .then((fetchData) => {
+    createButtons(fetchData);
   });
+
+function createButtons(data) {
+  Object.entries(data).forEach((item) => {
+    const button = document.createElement("button");
+    button.addEventListener("click", async () => {
+      const response = await fetch(item[1]);
+      const data = await response.json();
+      console.clear();
+      console.log(data);
+    });
+
+    container.appendChild(button);
+    button.innerText = item[0].toUpperCase();
+  });
+}
